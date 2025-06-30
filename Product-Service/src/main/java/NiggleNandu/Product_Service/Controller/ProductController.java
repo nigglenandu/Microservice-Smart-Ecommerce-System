7999b7932825dc4854f5b3c3d360b4d04d583f36
[Niggle Nandu] <nigglenandu@gmail.com>
2025-06-30 18:52:14 +0545

    product controller

---
+++ Product-Service/src/main/java/NiggleNandu/Product_Service/Controller/ProductController.java
@@ -0,0 +1,29 @@
+package NiggleNandu.Product_Service.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN') or hasRole('VENDOR')")
+    public ResponseEntity<ProductDto> create(@RequestBody ProductDto dto) {
+        return ResponseEntity.ok(productService.createProduct(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductDto>> all() {
+        return ResponseEntity.ok(productService.getAllActive());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDto> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.getProduct(id));
+    }
+}
