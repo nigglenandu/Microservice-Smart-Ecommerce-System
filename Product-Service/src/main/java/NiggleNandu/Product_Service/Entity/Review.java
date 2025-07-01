fa86a319b2f4f7dd6ee0e24f7691c218def29a3f
[Niggle Nandu] <nigglenandu@gmail.com>
2025-07-01 22:45:18 +0545

    review

---
+++ Product-Service/src/main/java/NiggleNandu/Product_Service/Entity/Review.java
@@ -0,0 +1,67 @@
+package NiggleNandu.Product_Service.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY);
+    private Long id;
+    private int rating;
+    private String comment;
+    private boolean approved;
+
+    @ManyToOne
+    private ProductEntity product;
+
+    public Review(int rating, ProductEntity product, Long id, String comment, boolean approved) {
+        this.rating = rating;
+        this.product = product;
+        this.id = id;
+        this.comment = comment;
+        this.approved = approved;
+    }
+
+    public Review() {
+    }
+
+    public boolean isApproved() {
+        return approved;
+    }
+
+    public void setApproved(boolean approved) {
+        this.approved = approved;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public ProductEntity getProduct() {
+        return product;
+    }
+
+    public void setProduct(ProductEntity product) {
+        this.product = product;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
