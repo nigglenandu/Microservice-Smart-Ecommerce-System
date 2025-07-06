362cf0917811bf2c5f1547eb20d4c9b150511daa
[Niggle Nandu] <nigglenandu@gmail.com>
2025-07-06 22:17:05 +0545

    cart item

--- Cart-Service/src/main/java/NiggleNandu/Cart_Service/Entity/CartEntity.java
+++ Cart-Service/src/main/java/NiggleNandu/Cart_Service/Entity/CartEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class CartEntity {
@@ -18,4 +19,54 @@ public class CartEntity {
     private Double discount;
     private LocalDateTime lastUpdated;
 
+    public CartEntity(Double discount, List<CartItem> items, LocalDateTime lastUpdated, Double total, String userId) {
+        this.discount = discount;
+        this.items = items;
+        this.lastUpdated = lastUpdated;
+        this.total = total;
+        this.userId = userId;
+    }
+
+    public CartEntity() {
+    }
+
+    public Double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Double discount) {
+        this.discount = discount;
+    }
+
+    public List<CartItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<CartItem> items) {
+        this.items = items;
+    }
+
+    public LocalDateTime getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(LocalDateTime lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 }
---
+++ Cart-Service/src/main/java/NiggleNandu/Cart_Service/Entity/CartItem.java
@@ -0,0 +1,69 @@
+package NiggleNandu.Cart_Service.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long productId;
+    private String variant;
+    private int quantity;
+    private double price;
+
+    public CartItem(Long id, double price, Long productId, int quantity, String variant) {
+        this.id = id;
+        this.price = price;
+        this.productId = productId;
+        this.quantity = quantity;
+        this.variant = variant;
+    }
+
+    public CartItem() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getVariant() {
+        return variant;
+    }
+
+    public void setVariant(String variant) {
+        this.variant = variant;
+    }
+}
