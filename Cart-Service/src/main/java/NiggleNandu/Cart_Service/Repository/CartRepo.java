f729651b56b9a00a47968decd028592905e2f3f6
[Niggle Nandu] <nigglenandu@gmail.com>
2025-07-08 17:26:08 +0545

    cart repo

---
+++ Cart-Service/src/main/java/NiggleNandu/Cart_Service/Repository/CartRepo.java
@@ -0,0 +1,8 @@
+package NiggleNandu.Cart_Service.Repository;
+
+import NiggleNandu.Cart_Service.Entity.CartEntity;
+import NiggleNandu.Cart_Service.Entity.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepo extends JpaRepository<CartEntity, Long> {
+}
