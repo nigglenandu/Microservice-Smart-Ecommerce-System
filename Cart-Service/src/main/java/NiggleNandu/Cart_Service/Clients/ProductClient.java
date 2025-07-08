9eb31da2534ba5dfce1a1020004a220db73eaddc
[Niggle Nandu] <nigglenandu@gmail.com>
2025-07-08 17:14:15 +0545

    dto product and variant

--- Cart-Service/src/main/java/NiggleNandu/Cart_Service/Clients/ProductClient.java
+++ Cart-Service/src/main/java/NiggleNandu/Cart_Service/Clients/ProductClient.java
@@ -1,11 +1,12 @@
 package NiggleNandu.Cart_Service.Clients;
 
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import NiggleNandu.Cart_Service.Dto.ProductDto;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@EnableFeignClients
-public class ProductClient {
+@FeignClient(name = "product-service")
+public interface ProductClient {
     @GetMapping("/api/products/{id}")
     ProductDto getProduct(@PathVariable("id") Long id);
 }
---
+++ Cart-Service/src/main/java/NiggleNandu/Cart_Service/Dto/ProductDto.java
@@ -0,0 +1,88 @@
+package NiggleNandu.Cart_Service.Dto;
+
+import java.util.List;
+
+public class ProductDto {
+    private Long id;
+
+    private String name;
+    private String description;
+    private Double price;
+    private String imageUrl;
+    private boolean active;
+    private List<ProductVariantDto> variants;
+    private Long categoryId;
+    private int stock;
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public List<ProductVariantDto> getVariants() {
+        return variants;
+    }
+
+    public void setVariants(List<ProductVariantDto> variants) {
+        this.variants = variants;
+    }
+}
