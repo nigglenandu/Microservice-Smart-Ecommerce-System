26b9165c9102ed114549f0f614e73c3208d7536a
[Niggle Nandu] <nigglenandu@gmail.com>
2025-07-19 16:44:44 +0545

    cart client in order

---
+++ Order-Service/src/main/java/NiggleNandu/Order_Service/Clients/CartClient.java
@@ -0,0 +1,11 @@
+package NiggleNandu.Order_Service.Clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient("cart-service")
+public interface CartClient {
+    @GetMapping("api/cart/{userId}")
+    CartDto getCart(@PathVariable String userId);
+}
