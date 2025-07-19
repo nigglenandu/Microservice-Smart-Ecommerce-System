c5959e09564d75d26e08c8ac271a5cb68f7fc8c2
[Niggle Nandu] <nigglenandu@gmail.com>
2025-07-19 16:37:07 +0545

    all dto order

---
+++ Order-Service/src/main/java/NiggleNandu/Order_Service/Dtos/OrderItemDto.java
@@ -0,0 +1,68 @@
+package NiggleNandu.Order_Service.Dtos;
+
+public class OrderItemDto {
+    private Long id;
+
+    private Long productId;
+    private String name;
+    private String size;
+    private String color;
+    private int quantity;
+    private double price;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
---
+++ Order-Service/src/main/java/NiggleNandu/Order_Service/Dtos/PlaceOrderRequest.java
@@ -0,0 +1,22 @@
+package NiggleNandu.Order_Service.Dtos;
+
+public class PlaceOrderRequest {
+    private String userId;
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
