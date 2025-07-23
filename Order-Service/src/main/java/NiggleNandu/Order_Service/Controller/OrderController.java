b1ce6947869bc867cc792ead1f52013306e39ba6
[Niggle Nandu] <nigglenandu@gmail.com>
2025-07-23 22:19:35 +0545

    order controller

---
+++ Order-Service/src/main/java/NiggleNandu/Order_Service/Controller/OrderController.java
@@ -0,0 +1,27 @@
+package NiggleNandu.Order_Service.Controller;
+
+import NiggleNandu.Order_Service.Dtos.OrderDto;
+import NiggleNandu.Order_Service.Services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping("/{userId}")
+    public ResponseEntity<OrderDto> place(@PathVariable String userId) {
+        return ResponseEntity.ok(orderService.placeOrder(userId));
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<List<OrderDto>> getByUser(@PathVariable String userId) {
+        return ResponseEntity.ok(orderService.getOrdersByUser(userId));
+    }
+}
