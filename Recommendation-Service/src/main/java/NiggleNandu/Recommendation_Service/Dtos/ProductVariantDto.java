3e803839bbe66b18160bd6b409a7335f387e61ec
[Niggle Nandu] <nigglenandu@gmail.com>
2025-07-26 23:25:56 +0545

    product dto and variant in recommendation service

---
+++ Recommendation-Service/src/main/java/NiggleNandu/Recommendation_Service/Dtos/ProductVariantDto.java
@@ -0,0 +1,24 @@
+package NiggleNandu.Recommendation_Service.Dtos;
+
+public class ProductVariantDto {
+    private String size;
+    private String color;
+
+    public ProductVariantDto() {}
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+}
