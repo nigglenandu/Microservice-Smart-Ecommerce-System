9864e121943c66acedec7afe7dfa543d8c8d7069
[Niggle Nandu] <nigglenandu@gmail.com>
2025-07-27 20:48:39 +0545

    recommendation controller

---
+++ Recommendation-Service/src/main/java/NiggleNandu/Recommendation_Service/Controller/RecommendationController.java
@@ -0,0 +1,29 @@
+package NiggleNandu.Recommendation_Service.Controller;
+
+import NiggleNandu.Recommendation_Service.Dtos.RecommendationDto;
+import NiggleNandu.Recommendation_Service.Service.RecommendationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/recommendations")
+public class RecommendationController {
+    @Autowired
+    private RecommendationService recommendationService;
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<RecommendationDto>> userRecommendations(@PathVariable String userId){
+        return ResponseEntity.ok(recommendationService.getUserRecommendations(userId));
+    }
+
+    @GetMapping("/trending")
+    public ResponseEntity<List<RecommendationDto>> trendingRecommendations(){
+        return ResponseEntity.ok(recommendationService.getTrendingProducts());
+    }
+}
