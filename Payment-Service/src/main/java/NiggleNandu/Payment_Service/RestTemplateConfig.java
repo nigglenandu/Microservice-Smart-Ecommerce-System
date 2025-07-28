840ea28c34992fd7a36c764d686ca4f978bf30cf
[Niggle Nandu] <nigglenandu@gmail.com>
2025-07-28 22:50:28 +0545

    rest template config

---
+++ Payment-Service/src/main/java/NiggleNandu/Payment_Service/RestTemplateConfig.java
@@ -0,0 +1,15 @@
+package NiggleNandu.Payment_Service;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+    public RestTemplate restTemplate(){
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(5000);
+        factory.setReadTimeout(5000);
+        return new RestTemplate(factory);
+    }
+}
