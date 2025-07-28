520caba2cd0804658d1047b4ec26ee114f39c823
[Niggle Nandu] <nigglenandu@gmail.com>
2025-07-28 22:47:48 +0545

    payment form field response

---
+++ Payment-Service/src/main/java/NiggleNandu/Payment_Service/PaymentFormFieldResponse.java
@@ -0,0 +1,9 @@
+package NiggleNandu.Payment_Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PaymentFormFieldResponse {
+    private String esewaUrl;
+    private Map<String, String> FormField = new HashMap<>();
+}
