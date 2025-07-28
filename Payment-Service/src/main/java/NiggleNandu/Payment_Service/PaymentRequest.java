b53f63fa6b6c9b0ff081b0f88cbeb7bec6c83b57
[Niggle Nandu] <nigglenandu@gmail.com>
2025-07-28 22:45:15 +0545

    payment reuqest

---
+++ Payment-Service/src/main/java/NiggleNandu/Payment_Service/PaymentRequest.java
@@ -0,0 +1,10 @@
+package NiggleNandu.Payment_Service;
+
+public class PaymentRequest {
+    private double amount;
+    private double taxAmount;
+    private Long orderId;
+    private double serviceCharge;
+    private double deliveryCharge;
+    private String productCode;
+}
