cfd32589cebea748abeb084eaa9360530dcf677d
[Niggle Nandu] <nigglenandu@gmail.com>
2025-08-16 15:36:06 +0545

    gateway

---
+++ gateway/src/main/java/com/example/gateway/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.gateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/api/accounts/**", "/api/funds/**").authenticated()
+                        .anyExchange().permitAll())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt())
+                .csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+}
